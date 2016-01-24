5fc9ff5c753f8f607f11648b5da66a11aae09b27
dixonwille <dixonwille@hotmail.com>
2016-01-23 22:37:34 -0500

    Corrected Cleanup Code

--- pulse/cmd/pulse/main.go
+++ pulse/cmd/pulse/main.go
@@ -58,14 +58,15 @@ func startPulse(filenames []string) {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	// On keyboard interrup cleanup the program
-	go func(i chan string) {
+	go func() {
 		for _ = range c {
 			fmt.Println("Exiting for Keyboard Interupt")
-			close(i)
-			email.DumpBuffer()
+			cleanUp()
 			os.Exit(0)
 		}
-	}(stdIn)
+	}()
+
+	defer cleanUp()
 
 	pulse.Run(stdIn, email.Send)
 	for _, filename := range filenames {
@@ -76,6 +77,10 @@ func startPulse(filenames []string) {
 		}
 	}
 	close(stdIn)
+}
+
+func cleanUp() {
+	email.DumpBuffer()
 }
 
 func checkList(filenames []string) {
